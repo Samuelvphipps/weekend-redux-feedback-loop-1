f7e09bf985eda64eb659b461cdd0299e3c7c344f
AlinaTrukhina <alina.trukhina@gmail.com>
2022-10-30 11:06:08 -0500

    add post route to record feedback

---
+++ server/routes/feedback.router.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../modules/pool');
+
+router.post('/', (req, res) => {
+    console.log('in POST request');
+    const sqlText = `
+        INSERT INTO feedback ("feeling", "understanding", 
+        "support", "comments")
+        VALUES($1, $2, $3, $4);`
+    const sqlParams = [
+        req.body.feelingRating, 
+        req.body.understandingRating,
+        req.body.supportedRating, 
+        req.body.commentsRating
+    ]
+    pool.query(sqlText, sqlParams)
+    .then((result) => {
+        res.sendStatus(201);
+    }).catch((error) => {
+        console.log('Error in POST feedbck', error)
+        res.sendStatus(500);
+    });
+})
+
+module.exports = router;
--- src/components/ReviewPage/ReviewPage.jsx
+++ src/components/ReviewPage/ReviewPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
+import axios from 'axios';
 
 function ReviewPage() {
 
@@ -11,9 +12,10 @@ function ReviewPage() {
     const supportedRating = useSelector((store) => store.supportedRating);
     const commentsRating = useSelector((store) => store.commentsRating);
 
-    const onSubmit = (evt) => {
-        evt.preventDefault();
+    const onSubmit = () => {
+        //evt.preventDefault();
 
+        // create object to send to database
         const ratings = {
             feelingRating: feelingRating,
             understandingRating: understandingRating,
@@ -21,7 +23,19 @@ function ReviewPage() {
             commentsRating: commentsRating
         }
 
-        
+        // POST feedback endpoint
+        axios({
+            method: 'POST',
+            url: '/',
+            data: ratings
+        })
+        .then((response) => {
+            console.log('feedback recorded');
+        })
+        .catch((error) => {
+            console.error('error posting feedback', error);
+        });
+        //history.push('/feedback');
     }
 
     return (
@@ -31,7 +45,7 @@ function ReviewPage() {
             <h2>Understanding: {understandingRating}</h2>
             <h2>Support: {supportedRating}</h2>
             <h2>Comments: {commentsRating}</h2>
-            <button className='submitBtn' onSubmit={(evt)=>onSubmit(evt)} type='submit'>
+            <button className='submitBtn' onClick={(evt)=>onSubmit(evt)} type='submit'>
                 Submit
             </button>
         </>
